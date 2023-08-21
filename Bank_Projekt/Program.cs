3a4c4b172290e5d7120a796be1e63ab345d0c395
Oliver Hannappel <oliver.hannappel@wmit.biz>
2023-08-21 14:54:47 +0200

    Add project files.

---
+++ Bank_Projekt/Program.cs
@@ -0,0 +1,9 @@
+﻿class Customer
+{
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public int Age { get; set; }
+    public string Adress { get; set; }
+    public int Adressnumber { get; set; }
+    public DateTime Birthday { get; set; }
+}
